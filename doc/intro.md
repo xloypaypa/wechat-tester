aa747b7006a065632314982eedd583772fd8b533
xsu <xsu@thoughtworks.com>
2016-08-19 21:17:19 +0530

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wechat-tester
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/wechat_tester/core_test.clj
@@ -0,0 +1,7 @@
+(ns wechat-tester.core-test
+  (:require [clojure.test :refer :all]
+            [wechat-tester.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
